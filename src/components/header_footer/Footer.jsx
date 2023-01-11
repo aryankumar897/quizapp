0e94292e69f93686bf77e7cb775a0f2b49306b81
aryankumar897 <82169659+aryankumar897@users.noreply.github.com>
2023-01-11 13:35:34 +0530

    first commit

---
+++ src/components/header_footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Footer = () => {
+    return (
+        <footer className="bck_black" style={{ marginTop:'100px' }}>
+            <Fade delay={500}>
+                <div className="font_righteous footer_logo_venue">React Quiz</div>
+                <div className="footer_copyright">
+                    Made with  
+                <span style={{color : 'crimson'}}>♥</span> By 
+                <a href="https://github.com/aryankumar897" target="_blank"           rel="noopener noreferrer"> group23
+                
+                </a>
+                
+                </div>
+            </Fade>
+        </footer>
+    );
+};
+
+export default Footer;
